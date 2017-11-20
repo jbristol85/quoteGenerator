41140fcc4a294edc0e32c037ad70bb263089e334
Josh Bristol <josh.bristol2@gmail.com>
2017-11-20 05:32:59 +0000

    Completed Project

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 /*global $ */
 
-var mashAPI = "";
+//Please copy and paste your API key between the quotes! //
+// var mashAPI = "";
+
+
+
 var quotes = {
+  mashAPI: "",
   quote: "",
   title: "",
   image: "",
   getQuote: function() {
     $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(data) {
       console.log(data);
-      quotes.quote = data[0].content;
+      quotes.quote = data[0].content.slice(3, -5);
       quotes.title = data[0].title;
       quotes.postQuote();
       quotes.getImage();
@@ -16,12 +21,12 @@ var quotes = {
   },
   getQuoteMash: function() {
     $.ajax({
-      url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+      url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
       type: "GET",
       data: {},
       datatype: 'json',
       success: function(data) {
-        console.log(data);
+        // console.log(data);
         quotes.quote = data.quote;
         quotes.title = data.author;
         quotes.postQuote();
@@ -29,13 +34,13 @@ var quotes = {
       },
       error: function(err) { alert(err); },
       beforeSend: function(xhr) {
-        xhr.setRequestHeader("X-Mashape-Authorization", mashAPI);
+        xhr.setRequestHeader("X-Mashape-Authorization", quotes.mashAPI);
       }
     });
   },
   postQuote: function() {
-    document.getElementById("insertQuote").innerHTML = quotes.quote;
-    document.getElementById("insertTitle").innerHTML = quotes.title;
+    document.getElementById("insertQuote").innerHTML = ('"' + quotes.quote + '"');
+    document.getElementById("insertTitle").innerHTML = '- ' + quotes.title;
   },
   postImg: function() {
     console.log(quotes.image);
@@ -47,7 +52,7 @@ var quotes = {
       url: "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=250&pilimit=20&wbptterms=description&gpssearch=${" + quotes.title + "}&gpslimit=20",
       dataType: "jsonp",
       success: function(data) {
-        console.log(data);
+        // console.log(data);
         if (data.query) {
           if (data.query.pages[0].hasOwnProperty("thumbnail")) {
             quotes.image = data.query.pages[0].thumbnail.source;
@@ -61,15 +66,18 @@ var quotes = {
       }
     });
   },
-  postTwitter: function(){
-    var slicedQuote = quotes.quote.slice(3,-5);
-    // console.log(slicedQuote);
-    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"'+slicedQuote+'"'+ '  - '+quotes.title));
+  postTwitter: function() {
+      window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quotes.quote + '"' + '  - ' + quotes.title));
   },
-  postReddit: function(){
-    var slicedQuote = quotes.quote.slice(3,-5);
-    window.open('https://www.reddit.com/r/quotes/submit?amp;title=Quote by: '+quotes.title+"&text="+slicedQuote);
-    
+  postReddit: function() {
+    window.open("https://www.reddit.com/r/quotes/submit?amp;title=Quote by: " + quotes.title + "&text=" + quotes.quote);
+  },
+  getRandomColor: function() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.body.style.backgroundColor = rgb;
   }
 };
 
@@ -79,12 +87,32 @@ document.getElementById("newQuote").onclick = function() {
   document.getElementById("insertImg").style.display = "none";
   document.getElementById("postTwit").style.display = "inline-block";
   document.getElementById("postRed").style.display = "inline-block";
+  document.body.classList.remove("movie");
+
   quotes.getQuote();
+  quotes.getRandomColor();
+};
+document.getElementById("newQuoteMash").onclick = function() {
+  document.getElementById("insertImg").style.display = "none";
+  document.getElementById("postTwit").style.display = "inline-block";
+  document.getElementById("postRed").style.display = "inline-block";
+  document.body.classList.add("movie");
+  
+  quotes.getQuoteMash();
 };
 
-document.getElementById("postTwit").onclick = function(){
+
+document.getElementById("postTwit").onclick = function() {
   quotes.postTwitter();
 };
-document.getElementById("postRed").onclick = function(){
+document.getElementById("postRed").onclick = function() {
   quotes.postReddit();
 };
+
+document.getElementById("submitAPI").onclick = function() {
+  quotes.mashAPI = document.getElementById("inputAPI").value;
+  document.getElementById("newQuoteMash").style.display = "inline-block";
+  document.getElementById("APISection").style.display = "none";
+  
+};
+quotes.getQuote();

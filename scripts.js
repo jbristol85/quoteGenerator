87cb58df1de3ff0c4f02c086ac711336b59b3be7
Josh Bristol <josh.bristol2@gmail.com>
2017-11-20 03:08:56 +0000

    Working project, no CSS

--- scripts.js
+++ scripts.js
@@ -10,27 +10,25 @@ var quotes = {
       console.log(data);
       quotes.quote = data[0].content;
       quotes.title = data[0].title;
-      quotes.getImage();
-      // quotes.postImg();
       quotes.postQuote();
+      quotes.getImage();
     });
   },
-  getQuoteMash: function(){
+  getQuoteMash: function() {
     $.ajax({
       url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
       type: "GET",
-      data:{},
+      data: {},
       datatype: 'json',
-      success: function(data){
+      success: function(data) {
         console.log(data);
         quotes.quote = data.quote;
         quotes.title = data.author;
         quotes.postQuote();
         quotes.getImage();
-        quotes.postImg();
       },
-      error: function(err) {alert(err);},
-      beforeSend: function(xhr){
+      error: function(err) { alert(err); },
+      beforeSend: function(xhr) {
         xhr.setRequestHeader("X-Mashape-Authorization", mashAPI);
       }
     });
@@ -39,10 +37,10 @@ var quotes = {
     document.getElementById("insertQuote").innerHTML = quotes.quote;
     document.getElementById("insertTitle").innerHTML = quotes.title;
   },
-  postImg: function(){
+  postImg: function() {
     console.log(quotes.image);
     document.getElementById("insertImg").src = quotes.image;
-    
+
   },
   getImage: function() {
     $.ajax({
@@ -50,25 +48,43 @@ var quotes = {
       dataType: "jsonp",
       success: function(data) {
         console.log(data);
-        if (data.query.pages[0].thumbnail) {
-          quotes.image = data.query.pages[0].thumbnail.source;
-          document.getElementById("insertImg").style.display = "inline-block";
-          quotes.postImg();
-        }else {
-          // quotes.image = "";
+        if (data.query) {
+          if (data.query.pages[0].hasOwnProperty("thumbnail")) {
+            quotes.image = data.query.pages[0].thumbnail.source;
+            document.getElementById("insertImg").style.display = "inline-block";
+            quotes.postImg();
+          }
+        }
+        else {
           document.getElementById("insertImg").style.display = "none";
         }
       }
     });
   },
+  postTwitter: function(){
+    var slicedQuote = quotes.quote.slice(3,-5);
+    // console.log(slicedQuote);
+    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"'+slicedQuote+'"'+ '  - '+quotes.title));
+  },
+  postReddit: function(){
+    var slicedQuote = quotes.quote.slice(3,-5);
+    window.open('https://www.reddit.com/r/quotes/submit?amp;title=Quote by: '+quotes.title+"&text="+slicedQuote);
+    
+  }
 };
 
 // a6wERDhjTmmshKyPGxqw4lMfF1Hjp1KLZxzjsnxE0IZIiQruQw
 
 document.getElementById("newQuote").onclick = function() {
+  document.getElementById("insertImg").style.display = "none";
+  document.getElementById("postTwit").style.display = "inline-block";
+  document.getElementById("postRed").style.display = "inline-block";
   quotes.getQuote();
-  
-  
 };
 
-// https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=
+document.getElementById("postTwit").onclick = function(){
+  quotes.postTwitter();
+};
+document.getElementById("postRed").onclick = function(){
+  quotes.postReddit();
+};
